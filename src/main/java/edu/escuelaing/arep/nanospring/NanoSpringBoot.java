2ca1ec58d213019543bb15f132c5126f483aed15
Germán Ospina <german.ospina-q@mail.escuelaing.edu.co>
2021-02-25 14:21:40 -0500

    Iniciar servidor con el procesador y la rutas definidas

--- src/main/java/edu/escuelaing/arep/nanospring/NanoSpringBoot.java
+++ src/main/java/edu/escuelaing/arep/nanospring/NanoSpringBoot.java
@@ -1,13 +1,22 @@
 package edu.escuelaing.arep.nanospring;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 
-public class NanoSpringBoot {
+import edu.escuelaing.arep.httpserver.HttpServer;
+import edu.escuelaing.arep.httpserver.Processor;
+
+public class NanoSpringBoot implements Processor{
 	
 	private static NanoSpringBoot _instance = new NanoSpringBoot();
 	private Map<String, Method> requestProcessors = new HashMap();
+	private HttpServer hserver;
 	
 	private NanoSpringBoot() {
 		
@@ -30,4 +39,42 @@ public class NanoSpringBoot {
 			}
 		}
 	}
+	@Override
+	public String handle(String path, HttpRequest req, HttpResponse res) {
+		String resp = "";
+		if(requestProcessors.containsKey(path)) {
+			try {
+				resp = requestProcessors.get(path).invoke(null, null).toString();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return this.validOkHttpHeader(resp);
+	}
+	
+	public String validOkHttpHeader(String resp) {
+		return "HTTP/1.1 200 OK\r\n"
+		        + "Content-Type: text/html\r\n"
+		         + "\r\n" + resp;
+	}
+	
+	public void startServer() throws FileNotFoundException, IOException, InterruptedException {
+		hserver = new HttpServer();
+		hserver.registerProcessor("/springapp", this);
+		hserver.startServer(8080);
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException, ClassNotFoundException {
+		NanoSpringBoot.getInstance().loadComponents(args);
+		NanoSpringBoot.getInstance().startServer();
+	}
+	
 }
